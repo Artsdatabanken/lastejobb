486d33adbf3cebc0571d6f3d35ed1d43c7f8d93f
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-06 21:04:31 +0200

    Process.execute funksjon

--- init.js
+++ init.js
@@ -1,24 +1,7 @@
-const { spawnSync } = require("child_process");
-const { log } = require("./lib");
+const { log, process } = require("./lib");
 const fs = require("fs");
 
-function exec(cmd, args) {
-  const r = spawnSync(cmd, args, {
-    encoding: "buffer",
-    shell: true,
-    stdio: [0, "pipe", "pipe"]
-  });
-  r.stdout
-    .toString()
-    .split("\n")
-    .forEach(line => {
-      if (line.trim().length > 0) log.info(line);
-    });
-  if (r.status > 0) {
-    log.error(r.stderr);
-    process.exit(1);
-  }
-}
+const exec = process.exec;
 
 const scripts = {
   download: "node index download",
