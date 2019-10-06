92db8d8f70f1e574f9fa9dd49b70f58beb5ce0c3
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-06 21:19:40 +0200

    Simplify

--- index.js
+++ index.js
@@ -2,14 +2,28 @@
 const lib = require("./lib");
 const init = require("./init");
 const { io, log, processes } = lib;
+const path = require("path");
 
-function kjørLastejobb(sciptFile) {
-  if (sciptFile.indexOf(".test") >= 0) return;
-  const ext = path.parse(sciptFile);
+function runJavascript(jsFile) {
+  processes.exec("node", ["--max_old_space_size=2096", `"${jsFile}"`]);
+}
+
+function runShellscript(shFile) {
+  processes.exec(shFile);
+}
+
+const filtypeLaunch = {
+  ".js": runJavascript,
+  ".sh": runShellscript
+};
+
+function kjørLastejobb(scriptFile) {
+  if (scriptFile.indexOf(".test") >= 0) return;
+  const ext = path.parse(scriptFile).ext;
   const launcher = filtypeLaunch[ext];
-  log.debug("Kjører " + sciptFile);
-  const status = launcher(sciptFile);
-  if (status > 0) process.exit(1);
+  if (!launcher) return log.warn("Mangler funksjon for å kjøre " + scriptFile);
+  log.debug("Kjører " + scriptFile);
+  launcher(scriptFile);
 }
 
 function kjørLastejobberUnder(rotkatalog) {
@@ -30,22 +44,4 @@ module.exports = {
   ...lib,
   kjørLastejobberUnder,
   kjørLastejobb
-};
-
-function runJavascript(fn) {
-  const r = processes.exec("node", [
-    "--max_old_space_size=2096",
-    `"${jsFile}"`
-  ]);
-  return r.status;
-}
-
-function runShellscript(fn) {
-  const r = processes.exec(fn);
-  return r.status;
-}
-
-const filtypeLaunch = {
-  ".js": runJavascript,
-  ".sh": runShellscript
 };
