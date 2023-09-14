13a4d06fcd37a43fcef8875095a1600766fa1ff9
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2023-09-14 13:16:25 +0200

    fix: no such file or directory when running tests

--- test/archive.test.js
+++ test/archive.test.js
@@ -1,8 +1,7 @@
 const { archive, io } = require("../lib");
 
-test("download and untar tar", () => {
-    archive.downloadAndUntar("https://github.com/Artsdatabanken/kommune/releases/latest/download/artifacts.tar")
+test("download and untar tar", async () => {
+    await archive.downloadAndUntar("https://github.com/Artsdatabanken/kommune/releases/latest/download/artifacts.tar")
     const actual = io.lesTempJson("artifacts/fylke.schema.json")
-    expect(actual).toMatchSnapshot();
+    expect(actual.$schema).toMatchSnapshot();
 });
-
