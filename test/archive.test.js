f006c6f892826e6383e55c06f1ebf4e70fd10135
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2023-09-15 13:44:48 +0200

    Test gzipped tar

--- test/archive.test.js
+++ test/archive.test.js
@@ -1,7 +1,7 @@
 const { archive, io } = require("../lib");
 
-test("download and untar tar", async () => {
-    await archive.downloadAndUntar("https://github.com/Artsdatabanken/kommune/releases/latest/download/artifacts.tar")
-    const actual = io.lesTempJson("artifacts/fylke.schema.json")
+test("download and untar gzipped tar", async () => {
+    await archive.downloadAndUntar("https://github.com/Artsdatabanken/kommune/releases/latest/download/kommune.tar.gz")
+    const actual = io.lesTempJson("kommune/fylke.schema.json")
     expect(actual.$schema).toMatchSnapshot();
 });
